31387435f7f638682d43caf8f7b08e7e880379c8
MatteCasla <matteocalandri23@gmail.com>
2026-02-22 11:36:32 -0300

    reorganizacion del codigo

--- monitoreo.py
+++ monitoreo.py
@@ -5,6 +5,9 @@ class Monitoreo:
     def obtener_cpu():
         return psutil.cpu_percent(interval=None)
     
+    def cpu_cores():
+        return psutil.cpu_count()
+
     @staticmethod
     def obtener_ram():
         return psutil.virtual_memory().percent
@@ -12,17 +15,3 @@ class Monitoreo:
     @staticmethod
     def obtener_disc():
         return psutil.disk_usage('c://').percent
-
-class Procesos:
-    @staticmethod
-    def obtener_procesos(attrs=None):
-        if attrs is None:
-            attrs = ['pid', 'name', 'memory_percent', 'cpu_percent']
-
-        procesos = []
-        for p in psutil.process_iter(attrs):
-            try:
-                procesos.append(p.info)
-            except (psutil.NoSuchProcess, psutil.AccessDenied):
-                pass
-        return procesos
